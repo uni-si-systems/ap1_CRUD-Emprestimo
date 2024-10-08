7f31ffa90feba3aa7cd719ce8d055158fa4270ae
Eris Pinheiro <erisap@gmail.com>
2024-10-08 18:58:50 -0300

    adição cli de navegação e atualização README.md

--- backend/routes.js
+++ backend/routes.js
@@ -14,19 +14,16 @@ const router = Router();
     })
 
     router.get('/clientes', selectClientes); // Seleciona todos os clientes
-    router.get('/cliente', selectCliente);   // Seleciona um cliente por ID (usando req.body.id)
     router.post('/clientes', insertClientes); // Insere um novo cliente
     router.put('/cliente', updateClientes); // Atualiza um cliente
     router.delete('/cliente', deleteCliente); // Deleta um cliente por ID (usando req.body.id)
 
     router.get('/veiculos', selectVeiculos); // Seleciona todos os veiculo
-    router.get('/veiculos', selectVeiculo);   // Seleciona um veiculo por ID (usando req.body.id)
     router.post('/veiculos', insertVeiculos); // Insere um novo veiculo
-    router.put('/veiculos', updateVeiculos); // Atualiza um veiculo
+    router.put('/veiculo', updateVeiculos); // Atualiza um veiculo
     router.delete('/veiculos', deleteVeiculo); // Deleta um veiculo por ID (usando req.body.id)
 
     router.get('/emprestimos', selectEmprestimos); // Seleciona todos os emprestimos
-    router.get('/emprestimos', selectEmprestimo);   // Seleciona um emprestimos por ID (usando req.body.id)
     router.post('/emprestimos', insertEmprestimo); // Insere um novo emprestimos
     router.put('/emprestimos', updateEmprestimo); // Atualiza um emprestimos
     router.delete('/emprestimos', deleteEmprestimos); // Deleta um emprestimos por ID (usando req.body.id)
