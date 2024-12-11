9fe1d9950b8558ebbe4362e8a9b413be62a142f8
Eris Pinheiro <erisap@gmail.com>
2024-12-11 15:28:07 -0300

    commit final banco

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ export async function openDb() {
     host: 'localhost', // Endereço do servidor MySQL
     user: 'root', // Usuário do banco de dados
     password: '12345678', // Senha do banco de dados
-    database: 'carnow', // Nome do banco de dados
+    database: 'drivenow', // Nome do banco de dados
   });
 
   console.log('Connected to MySQL database');
